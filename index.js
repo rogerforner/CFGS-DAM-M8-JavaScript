5b50205c33e9318991263b5051f204f240a5f60c
Roger Forner Fabre <sir.rogerforner@gmail.com>
2016-09-28 19:59:07 +0200

    funcions

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 
-console.log("Hello world at external file");
+//Això no es pot fer en php. HOYSTER. Primer carrega en memoria funcions i variables.
 
-//alert('Hola mon!'); //Evitar utilitzar aquesta forma!
+hola('roger');
 
-$name= 'roger';
+function hola($name) {
+    console.log('Hola'+$name);
+}
 
-//Windows: només en navegadors!!
-window.alert(window.$name);
+
+//
+// console.log("Hello world at external file");
+//
+// //alert('Hola mon!'); //Evitar utilitzar aquesta forma!
+//
+// $name= 'roger';
+//
+// //Windows: només en navegadors!!
+// window.alert(window.$name);
 
 //Objecte window : objecte per defecte en navegador = Namespace Global
 //Tothom depèn d'aquest objecte.
