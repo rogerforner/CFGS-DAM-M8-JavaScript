5d3994a9ccc09d5a559f5cc276692661ffea64ed
Roger Forner Fabre <sir.rogerforner@gmail.com>
2016-09-28 19:52:10 +0200

    window object

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 
 console.log("Hello world at external file");
 
-alert('Hola mon!'); //Evitar utilitzar aquesta forma!
+//alert('Hola mon!'); //Evitar utilitzar aquesta forma!
+
+$name= 'roger';
+
+//Windows: només en navegadors!!
+window.alert(window.$name);
+
+//Objecte window : objecte per defecte en navegador = Namespace Global
+//Tothom depèn d'aquest objecte.
+
+// Scope d'una variable. Funció
+
+
+
+
