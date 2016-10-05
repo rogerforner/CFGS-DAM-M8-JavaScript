38b2bedd9ed9c3d5cfca44e568b58b9cae3abf8a
Roger Forner Fabre <sir.rogerforner@gmail.com>
2016-10-05 19:39:43 +0200

    constructors (funcions)

---
+++ objectes.js
@@ -0,0 +1,15 @@
+// class { } -> Sugar Syntax
+
+// First class citizens functions
+//var x = function(){}
+// Utilitzarem les funcions per "fer" objectes
+
+//Javascript: functional programming
+
+function Persona() {
+    console.log('Creant una nova persona')
+} // function contructor
+
+var persona1 = new Persona();
+var persona2 = new Persona();
+
