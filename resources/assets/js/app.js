e3b1360d57d29583e7ebc1c7704c9ee6d6d1c581
Roger Forner Fabre <sir.rogerforner@gmail.com>
2016-10-07 17:22:49 +0200

    herència i composició

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,6 +8,17 @@ var Persona = function (name) {
     console.log("Creant un objecte");
 };
 
+//Composició. Extendre una funció/Herencia
+
+//Serveix per cambiar la funció original.
+Persona.prototype.sayHello = function(){
+    console.log('Hola '+ this.name);
+};
+
+console.log(Persona);
+
 var personeta = new Persona('roger');
 
 console.log(personeta.name);
+
+console.log(personeta.sayHello());
